32dbafb0ff14babac3268b4f7f4a48268ade4277
Babyflum <fatlumsadiku91@gmail.com>
2019-05-21 11:26:04 +0200

    updated according to protocol

--- indexer.py
+++ indexer.py
@@ -1,9 +1,13 @@
 import re
 import pickle
 import time
+import postings
+import gc
+# import pprint
+import sys
 
 inverted_index = {}
-
+counting_index = {}
 
 def read_file(file_name):
     """
@@ -15,67 +19,134 @@ def read_file(file_name):
     >>> ii = InvertedIndex()
     >>> ii.read_file("test.csv")
     >>> ii.file_dict['2']
-    ('1', ['How', 'do', 'you', 'charge', 'Per', 'click', 'Per', 'impression', 'Per', 'hour', 'We', 'charge', 'for', 'approximate', 'value', 'delivered'])
+    ('1', ['How', 'do', 'you', 'charge', 'Per', 'click',
+    ... 'Per', 'impression', 'Per', 'hour', 'We', 'charge',
+    ... 'for', 'approximate', 'value', 'delivered'])
     >>> ii.inverted_index['How']
     ['2', '3', '4']
     """
-    post2 = open(file_name, mode="r", encoding="utf8")
+    raw_data = open(file_name, mode="r", encoding="utf8")
     file_dict = {}
-    # format PostContent to accommodate for some errors such as
-    # "hey?No, hello!jim, " etc and remove punctuation
-    regex = re.compile("([.?!;:(),\[\]]+)(\w)")
-    regex2 = re.compile("[.?!;:]+")
-    regex3 = re.compile("(\w)([.?!;:(),\[\]]+)")
-
-    for row in post2.readlines():
-        # split == ",". Initial " remains
-        row = re.sub(regex, r" \2", row)
-        row = re.sub(regex3, r"\1 ", row)
-        row = re.sub(regex2, "", row)
-        row = re.sub(" {2}", " ", row)
-        line = row.split("\",\"")
-        if len(line) < 9:
+    andclean = re.compile(r'\&+')
+    contractionclean = re.compile(r'[\'`´]+')
+    cleanup = re.compile(r'[.?!;:(),/"]+|( - )')
+    # f = open('pctest.csv', mode='w')
+    for row in raw_data.readlines():
+        columns = row.split('\",\"')
+        if len(columns) < 9:
             # ignore rows with not enough columns
             pass
         else:
-            # initial " removed
-            line[0] = line[0][1:]
-            # dictionary will be of the form {ID: (MemberID, Wordlist of PostContent)}
-            file_dict[line[0]] = (line[5], line[8].split())
-
+            # remove initial quotation mark
+            ID = columns[0][1:]
+            postContent = columns[8]
+            memberID = columns[5]
+            # clean up the post content so that only words are left.
+            postContent = re.sub(cleanup, r' ', postContent)
+            # contractions like I'm are turned into Im.
+            # this is only a temporary solution.
+            postContent = re.sub(contractionclean, r'', postContent)
+            # ampersands are turned into and.
+            postContent = re.sub(andclean, r'and', postContent)
+            # whitespaces are singled.
+            postContent = re.sub(r'\s+', r' ', postContent)
+            # remove leading and trailing whitespaces.
+            postContent = postContent.strip()
+            file_dict[ID] = (memberID, postContent.split())
+            # just for test purposes
+            # write into a csv file all of the cleaned up post content.
+            # f.write(postContent + ';\n')
+    # f.close()
     return file_dict
 
 
-def generate_index(file_dict):
+def generate_index_new(file_dict, casefold=True, nonumbers=True):
     """
-    Generate Inverted Index as dictionary of form
-    {'word', list of IDs}.
-    >>> fd = {
-    '33':('3',['this','is','a','test']),
-    '345':('4',['repeat','this','repeat','this])
-    '346':('3',['another','test','another','another','another'])
-    '556':('4',['this','is','a','repeat','of','this])}
-    >>> ii = generate_index(fd)
-    >>> print(ii)
-    '{'this': [('33', [1]), ('345', [2,4])}'
+    Function that generates a temporary index in memory and once
+    it reaches a certain threshold, writes the temporary index
+    onto the actual inverted index and deletes the temporary index
+    from memory. This should save disk-read and disk_write time.
     """
+    tmp_index = dict()
+    prefilter = re.compile(r'[^a-zA-Z0-9-]+')
+    numberfilter = re.compile(r'[0-9]')
+    id_counter = 0
+    batchcounter = 1
     for ID in file_dict:
+        if id_counter % 10000 == 0:
+            print("{} IDs checked".format(id_counter))
+
+        # this part of the code cleans the temporary index
+        # all data is stored on disk and the inverted index is updated
+        # the temporary index is then deleted from memory and
+        # a new temporary index is generated in its stead.
+        if id_counter % 100000 == 0:
+            batchcounter += 1
+            print("BATCH TRANSMISSION Nr.{}".format(batchcounter-1))
+            for word in tmp_index:
+                if word not in inverted_index:
+                    # generate ID, position list tuple
+                    path = postings.write_postings(word, tmp_index[word],
+                                                   casefold)
+                    inverted_index[word] = path
+                elif word in inverted_index:
+                    lst = postings.read_postings(inverted_index[word], word)
+                    # since IDs are sorted in input
+                    # this only checks the last added ID
+                    lst += tmp_index[word]
+                    postings.write_postings(word, lst, casefold)
+            del(tmp_index)
+            tmp_index = dict()
+        
         # pos looks at position of word in wordlist
         pos = 0
         # go through wordlists
         for word in file_dict[ID][1]:
             pos += 1
-            if word not in inverted_index:
-                # generate ID, position list tuple
-                inverted_index[word] = [(ID, [pos])]
-            elif word in inverted_index:
-                # since IDs are sorted in input, this only checks the last added ID
-                if inverted_index[word][-1][0] == ID:
-                    inverted_index[word][-1][1].append(pos)
-                else:
-                    inverted_index[word].append((ID, [pos]))
+            if casefold is True:
+                word = word.casefold()
+            # filter out words which contain non-alphanumeric characters
+            # or start or end with a hyphen
+            if (re.search(prefilter, word) is not None or len(word) > 20
+                    or re.match(r'.+\-$|\-.+', word)):
+                continue
+            if nonumbers is True:
+                if re.search(numberfilter, word) is not None:
+                    continue
 
-    return inverted_index
+            # here we increment the counting index
+            if word in counting_index:
+                counting_index[word] += 1
+            else:
+                counting_index[word] = 1
+
+            # back to the original function
+            if word not in tmp_index:
+                # generate ID, position list tuple
+                tmp_index[word] = [[ID, [pos]]]
+            elif word in tmp_index:
+                # since IDs are sorted in input, this only checks the last added ID
+                if tmp_index[word][-1][0] == ID:
+                    tmp_index[word][-1][1].append(pos)
+                else:
+                    tmp_index[word].append([ID, [pos]])
+        id_counter += 1
+
+    # transmit remaining index
+    print("LAST BATCH TRANSMISSION")
+    for word in tmp_index:
+        if word not in inverted_index:
+            # generate ID, position list tuple
+            path = postings.write_postings(word, tmp_index[word],
+                                           casefold)
+            inverted_index[word] = path
+        elif word in inverted_index:
+            lst = postings.read_postings(inverted_index[word], word)
+            # since IDs are sorted in input
+            # this only checks the last added ID
+            lst += tmp_index[word]
+            postings.write_postings(word, lst, casefold)
+    del(tmp_index)
 
 
 def write_file(name):
@@ -99,7 +170,8 @@ def write_file(name):
 
 def pickle_file(name):
     """
-    Pickles (or serializes) hash table in folder for easy access by Search Function.
+    Pickles (or serializes) hash table in folder for
+    easy access by Search Function.
     """
 
     pickle_out = open("100k_index.pickle", "wb")
@@ -110,28 +182,19 @@ def pickle_file(name):
 if __name__ == "__main__":
     time1 = time.time()
     file_name = input("Name of File to be indexed:")
+    print("reading file")
     FD = read_file(file_name)
-    generate_index(FD)
-    pickle_file(inverted_index)
+    print("generating index")
+    generate_index_new(FD)
+    del(FD)
     time2 = time.time()
+    print("pickling index")
+    file = open('1Mii.pickle', mode='wb')
+    pickle.dump(inverted_index, file)
+    file.close()
     print("%.5f seconds" % (time2 - time1))
-    answer = input("Do you want to write a .csv file of the index? y/n:")
+    print("Size of index: %d" % sys.getsizeof(inverted_index))
 
-    while True:
-        if answer == ("y" or "Y"):
-            write_file(input("Name for .csv file:"))
-            break
-        elif answer != ("n" or "N"):
-            answer = input("Please only type in one of the following: y,Y,n,N:")
-        else:
-            break
-    '''
-    fd = {
-        '33': ('3', ['this', 'is', 'a', 'test']),
-        '345': ('4', ['repeat', 'this', 'repeat', 'this']),
-        '346': ('3', ['another', 'test', 'another', 'another', 'another']),
-        '556': ('4', ['this', 'is', 'a', 'repeat', 'of', 'this', 'and', 'this'])}
-    ii = generate_index(fd)
-
-    pprint.pprint(ii)
-    '''
+    ci_file = open('1Mci.pickle', mode='wb')
+    pickle.dump(counting_index, ci_file)
+    ci_file.close()
